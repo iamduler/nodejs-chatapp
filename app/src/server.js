a3e257c06fb643898b27594b8d0851c936dae2fc
lam.bui <lam.bui@cloudgo.vn>
2025-02-23 16:59:39 +0700

    Using emit() & on() to trigger & listen events in server/client

--- app/src/server.js
+++ app/src/server.js
@@ -11,16 +11,31 @@ app.use(express.static(publicPathDir));
 const server = http.createServer(app); // Khởi tạo server từ express & http
 const io = socketio(server); // Init socketIO with server
 
+var count = 1;
+const message = 'Chào mọi người';
+
 // Connection - lắng nghe sự kiện kết nối từ client nào đó
 io.on("connection", (socket) => {
     // Mỗi client sẽ được server cung cấp 1 socket
     console.log('New client connected.');
+
+    // Truyền giá trị count từ server -> client
+    // emit() giúp gửi dữ liệu theo sự kiện từ server -> client
+    socket.emit('Send count from server to client', count);
+
+    socket.emit('Send message from server to client', message);
 
     // Disconnection
     socket.on('disconnect', () => {
         // Khi client đóng tab/ đóng trình duyệt
         console.log('Client left server.');
     })
+
+    // Nhận sự kiện từ client
+    socket.on('Send increment from client to server', () => {
+        count++;
+        socket.emit('Send count from server to client', count);
+    });
 })
 
 const port = 3000;
