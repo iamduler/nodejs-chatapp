e24f90bedf6cbbcfc7aa36789ae5709197cbaa7c
lam.bui <lam.bui@cloudgo.vn>
2025-02-23 16:44:16 +0700

    Increments SocketIO

--- app/src/server.js
+++ app/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
 
@@ -8,6 +9,19 @@ const publicPathDir = path.join(__dirname, '../public');
 app.use(express.static(publicPathDir));
 
 const server = http.createServer(app); // Khởi tạo server từ express & http
+const io = socketio(server); // Init socketIO with server
+
+// Connection - lắng nghe sự kiện kết nối từ client nào đó
+io.on("connection", (socket) => {
+    // Mỗi client sẽ được server cung cấp 1 socket
+    console.log('New client connected.');
+
+    // Disconnection
+    socket.on('disconnect', () => {
+        // Khi client đóng tab/ đóng trình duyệt
+        console.log('Client left server.');
+    })
+})
 
 const port = 3000;
 server.listen(port, () => {
